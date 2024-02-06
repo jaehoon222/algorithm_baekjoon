73045df8db74ff4752c734285e9034545b79df67
jaehoon222 <jhoon5061@naver.com>
2024-02-06 16:04:25 +0900

    2024/02/6

---
+++ 1927.py
@@ -0,0 +1,20 @@
+import heapq
+
+n = int(input())
+
+result = []
+heap = []
+for _ in range(n):
+    x = int(input())
+    if x==0:
+        if len(heap)==0:
+            result.append(0)
+        else:
+            top = heapq.heappop(heap)
+            result.append(top)
+    else:
+        heapq.heappush(heap, x)
+
+
+for i in result:
+    print(i)
